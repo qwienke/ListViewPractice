ff46c3f2ad450fd085623134a9a8e293413e0c32
qwienke <qwienke13@gmail.com>
2024-02-03 02:32:32 -0700

    Basic list view

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Quinn Wienke on 2/3/24.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
